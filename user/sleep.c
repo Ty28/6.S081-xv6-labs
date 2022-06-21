39f3d1e0740dcb4e57f1c9cb231257d5809d5def
Ty28 <1574588673@qq.com>
2022-06-21 18:26:08 +0800

    finish sleep

---
+++ user/sleep.c
@@ -0,0 +1,19 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    int time = 0;
+    if(argc != 2)   // parameters error
+    {
+        fprintf(2, "usage: sleep [time]\n");
+        exit(1);
+    }
+    else
+    {
+        time = atoi(argv[1]);
+        sleep(time);
+    }
+    exit(0);
+}
